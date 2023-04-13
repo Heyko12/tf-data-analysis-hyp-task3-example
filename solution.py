7da9e4d70ce49e65de6b75434e7c4221a6590612
Natasha <puzir-125@mail.ru>
2023-04-14 00:02:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 2090817777
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(...) -> bool: 
+    alpha = 0.08
+    threshold = 300
+    mean = sum(data) / len(data)
+    std = stats.tstd(data)
+    n = len(data)
+    t_stat = (mean - threshold) / (std / (n**0.5))
+    crit_val = stats.t.ppf(alpha, df=n-1)
+    return t_stat < alpha
